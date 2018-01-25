9a8e688720f66c60430c8db5457e40e337d0aa9d
Aabhimanyu Gupta <abhimanyu10gupta@gmail.com>
2018-01-25 11:12:21 +1000

    Add QuizCard class

--- src/QuizCardBuilder.java
+++ src/QuizCardBuilder.java
@@ -1,4 +1,34 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.ArrayList;
+
+import javax.swing.*;
 
 public class QuizCardBuilder {
-
+	private JTextArea question;
+	private JTextArea answer;
+	private ArrayList<QuizCard> cardList;
+	
+	
+	public void go(){
+		
+	}
+	
+	public class NextCardListener implements ActionListener {
+		public void actionPerformed(ActionEvent e){
+			
+		}
+		
+	}
+	
+	public class SaveMenuListener implements ActionListener {
+		public void actionPerformed(ActionEvent e){
+			
+		}
+	}
+	
+	public void saveFile(File file){
+		
+	}
 }
